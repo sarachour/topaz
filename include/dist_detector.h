3f55d4efc28387fc648cc731ab0da060d6406d83
Sara AChour <sachour@mit.com>
2015-01-07 08:22:41 -0800

    added distribution manager

--- include/dist_detector.h
+++ include/dist_detector.h
@@ -1,20 +1,26 @@
 #ifndef DIST_DETECTOR
 #define DIST_DETECTOR
-
+#include "ctrl_system.h"
 #include "abs_detectors.h"
 //n dimensional sphere
 
 class Vector {
 	private:
 		int n;
-		float * e;
+		float * e; // only dealloc if not passed in.
+		float * ALLOC;
 	public:
+		Vector(float * e, int n);
+		Vector(int n);
+		Vector(const Vector& v);
+		~Vector();
 		void set(int i, float f);
+		void set(float f);
 		float* get();
-		void sub(Vector v);
-		void add(Vector v);
-		void dot(Vector v);
-		void mag(Vector v);
+		Vector* sub(const Vector v);
+		Vector* add(const Vector v);
+		const float dot(const Vector v);
+		const float mag();
 };
 
 template <class T>
@@ -31,22 +37,37 @@ class Distribution {
 	private:
 		Hypersphere<Vector> sigma; //standard deviation over
 		Vector mean;
+		ControlSystem * ctrl_f; //factors to consider
+		ControlSystem * ctrl_targ; //target rate to consider
 		float p_pt;
 		float p_fp;
 		int n; //n pts
 		static const float WT = 0.0001;
 		static const float WT_I = 0.9999;
 		float f; // number of standard deviations to consider.
+		float targ_fp;
 	public:
 		Distribution();
 		~Distribution();
-		void update(Vector pt); //update the mean, stddev appropriately
-		float dist(Vector pt); //how many stddevs a point is from the mean.
+		void update(const Vector pt); //update the mean, stddev appropriately
+		float dist(const Vector pt); //how many stddevs a point is from the mean.
+		void merge(const Distribution& d);
+		//statistics
+		void fp(){p_fp += WT;};
+		void decay_fp(){p_fp *= WT_I;}
+		void use(){p_pt += WT;}
+		void decay_use(){p_pt *= WT_I;}
+		
+		void update_ctrl();
+		
+		float thresh(){return f;}
 };
 class AbsDistDetector : public AbsDetector{
 	private:
 		Distribution * dists;
-		int n;
+		int n_dists;
+		int max_dists;
+		static const float EPS = 1E-23;
 	public:
 		AbsDistDetector(int n);
 		~AbsDistDetector();
--- src/common/detector/abs_dist_region_detector.cpp
+++ src/common/detector/abs_dist_region_detector.cpp
@@ -1,1 +1,83 @@
 #include "dist_detector.h"
+
+AbsDistDetector::AbsDistDetector(int n){
+	this->dists = new Distribution[n];
+	this->n_dists = 0;
+	this->max_dists = n;
+}
+AbsDistDetector::~AbsDistDetector(){
+	delete this->dists;
+}
+bool AbsDistDetector::test(){
+	Vector pt(this->data, this->n);
+	float lowest_score = -1;
+	int best_dist = -1;
+	//for each distribution
+	for(int i=0; i < this->n_dists; i++){
+		float sc = this->dists[i].dist(i);
+		float tsc = this->dists[i].thresh();
+		if(sc < tsc && (sc < lowest_score || best_dist == -1)){
+			best_dist = i;
+			lowest_score = sc;
+		}
+		this->dists[i].decay_use();
+	}
+	if(best_dist >= 0){
+		this->dists[best_dist].update(pt);
+		this->dists[best_dist].use();
+		return true;
+	}
+	else return false;
+}
+bool AbsDistDetector::train(){
+	Vector pt(this->data, this->n);
+	Vector ptk(this->data, this->n);
+	
+	float diff = pt.sub(ptk)->mag();
+	bool is_err = diff > EPS ? true : false;
+	
+	//add a new distribution
+	if(this->n_dists < this->max_dists){
+		int nu = this->n_dists;
+		//update distribution
+		this->dists[nu].update(ptk);
+		this->n_dists++;
+	}
+	float score=0;
+	float score_key=0;
+	int curr_dist = -1;
+	int curr_dist_key = -1;
+	//find closest existing distribution for key, error
+	for(int i=0; i < this->n_dists; i++){
+		float c_score = this->dists[i].dist(pt) - this->dists[i].thresh();
+		float c_score_k = this->dists[i].dist(ptk) - this->dists[i].thresh();
+		if(curr_dist < 0 || c_score < score){
+			curr_dist = i;
+			score = c_score;
+		}
+		if(curr_dist_key < 0 || c_score_k < score_key){
+			curr_dist_key = i;
+			score_key = i;
+		}
+	}
+	
+	//update distribution with false positive and update control system
+	this->dists[curr_dist].decay_fp();
+	if(!is_err){
+		this->dists[curr_dist].fp();
+	}
+	this->dists[curr_dist].update_ctrl();
+	
+	//update key distribution with use key
+	this->dists[curr_dist_key].use();
+	this->dists[curr_dist_key].update(ptk);
+	
+	return true;
+}
+
+void AbsDistDetector::log(){
+	
+}
+void AbsDistDetector::print(){
+	
+}
