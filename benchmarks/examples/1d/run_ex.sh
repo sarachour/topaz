c2d21a42b93589b636a173f402a4fd45e03d40e0
Sara AChour <sachour@mit.com>
2015-03-10 14:56:38 -0700

    changed things

--- benchmarks/examples/1d/run_ex.sh
+++ benchmarks/examples/1d/run_ex.sh
@@ -26,6 +26,7 @@ OUTDIR=$TAGS
 SUFFIX="$INPUT.$SEED"
 #sutil_run_script.sh run_ex.sh 1.100 1 reexec:t=scar,b=5,p=0.10 heavy-dram d
 #sutil_run_script.sh run_ex.sh 1.100 1 none heavy-dram 
+#sutil_run_script.sh run_ex.sh 1.100 1 reexec:t=dist,b=1,p=0.10 heavy-dram d
 
 ND=$(echo "$INPUT" | grep -o "^[0-9]*")
 NPTS=$(echo "$INPUT" | grep -o "[0-9]*$")
