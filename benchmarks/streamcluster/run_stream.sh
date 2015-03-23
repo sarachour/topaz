e6fd81226656bfd0d7982ea171f97c8634349539
Sara AChour <sachour@mit.com>
2015-03-23 16:49:19 -0700

    fixed some basic problems

--- benchmarks/streamcluster/run_stream.sh
+++ benchmarks/streamcluster/run_stream.sh
@@ -32,7 +32,7 @@ function updateDirectories {
 OUTDIR=$TAGS
 SUFFIX="$INPUT.$SEED"
 
-#sutil_run_script.sh run_bs.sh 16K 1 reexec:t=scar,b=5,p=0.10 heavy-dram d
+#sutil_run_script.sh run_stream.sh large 1 reexec:t=scar,b=5,p=0.10 heavy-dram d
 
 createDirectories $OUTDIR
 tpzrun $PIN_ARGS  -- src/streamcluster $TOPAZ_ARGS @ $MINCLUST $MAXCLUST $CHUNKSIZE $CLUSTERSIZE inputs/$INPUT.txt clust.txt 1 > log.txt
