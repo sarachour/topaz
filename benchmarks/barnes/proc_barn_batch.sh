4e5e6844b019632749301c47c52668e4d09383b1
Sara AChour <sachour@mit.com>
2015-07-13 14:59:48 -0700

    added script

--- benchmarks/barnes/proc_barn_batch.sh
+++ benchmarks/barnes/proc_barn_batch.sh
@@ -16,7 +16,7 @@ do
 	KIND="normal"
 	for efile in  `ls output/$folder/err*`
 	do
-		ERROR=$(cat $efile | grep -E "Percent Price:[ 0-9\.]+$" | grep -o -E "[0-9\.]+$")
+		ERROR=$(cat $efile | grep -E "Average Pct Vector Err:[ 0-9\.]+$" | grep -o -E "[0-9\.]+$")
 		#ERROR=$(cat $efile | grep -E "Number Errors:[ 0-9\.]+$" | grep -o -E "[0-9\.]+$")
 		ERRORS=$ERRORS","$ERROR
 	done
