3288458c60347244b2757736599bd648b535ea63
Sara AChour <sachour@mit.com>
2015-07-13 15:16:29 -0700

    added stream script

---
+++ benchmarks/streamcluster/run_stream_batch.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+INP=med
+TYP=static
+PROB=0.01
+BLOCKSIZE=5
+#STARTSEED=1
+#NSEEDS=3
+STARTSEED=1
+NSEEDS=8
+SCRIPT=run_stream
+
+FLAGS=
+
+echo "streamcluster has started executing." > tmp.txt
+echo "flags used: $FLAGS" >> tmp.txt
+echo "seeds: $STARTSEED -> $NSEEDS" >> tmp.txt
+echo "input: $INP" >> tmp.txt
+
+mutt -s "Barnes Started" sarachour@gmail.com  < tmp.txt
+
+for i in $(seq $STARTSEED $NSEEDS);
+do
+
+sutil_run_script.sh $SCRIPT.sh $INP $i reexec:t=scar,b=5,p=$PROB iact-med-$TYP $FLAGS
+sutil_run_script.sh $SCRIPT.sh $INP $i reexec:t=scar,b=4,p=$PROB iact-med-$TYP $FLAGS
+sutil_run_script.sh $SCRIPT.sh $INP $i reexec:t=scar,b=3,p=$PROB iact-med-$TYP $FLAGS
+sutil_run_script.sh $SCRIPT.sh $INP $i reexec:t=scar,b=2,p=$PROB iact-med-$TYP $FLAGS
+
+sutil_run_script.sh $SCRIPT.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.0 iact-med-$TYP $FLAGS
+sutil_run_script.sh $SCRIPT.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.01 iact-med-$TYP $FLAGS
+sutil_run_script.sh $SCRIPT.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.02 iact-med-$TYP $FLAGS
+sutil_run_script.sh $SCRIPT.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.04 iact-med-$TYP $FLAGS
+sutil_run_script.sh $SCRIPT.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.08 iact-med-$TYP $FLAGS
+
+
+done
+
+
+echo "streamcluster is finished executing." > tmp.txt
+echo "flags used: $FLAGS" >> tmp.txt
+echo "seeds: $STARTSEED -> $NSEEDS" >> tmp.txt
+echo "input: $INP" >> tmp.txt
+
+mutt -s "Barnes Finished" sarachour@gmail.com  < tmp.txt
