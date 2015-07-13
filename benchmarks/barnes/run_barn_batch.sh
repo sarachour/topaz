7029c205bc7805e686010b031f1c0ed5316c762b
Sara AChour <sachour@mit.com>
2015-07-13 12:54:17 -0700

    fixed genericity

--- benchmarks/barnes/run_barn_batch.sh
+++ benchmarks/barnes/run_barn_batch.sh
@@ -8,21 +8,23 @@ BLOCKSIZE=5
 #NSEEDS=3
 STARTSEED=1
 NSEEDS=8
-FLAGS=d
+SCRIPT=run_barn
+
+FLAGS=
 
 for i in $(seq $STARTSEED $NSEEDS);
 do
 
-sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=5,p=$PROB iact-med-$TYP $FLAGS
-sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=4,p=$PROB iact-med-$TYP $FLAGS
-sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=3,p=$PROB iact-med-$TYP $FLAGS
-sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=2,p=$PROB iact-med-$TYP $FLAGS
+sutil_run_script.sh $SCRIPT.sh $INP $i reexec:t=scar,b=5,p=$PROB iact-med-$TYP $FLAGS
+sutil_run_script.sh $SCRIPT.sh $INP $i reexec:t=scar,b=4,p=$PROB iact-med-$TYP $FLAGS
+sutil_run_script.sh $SCRIPT.sh $INP $i reexec:t=scar,b=3,p=$PROB iact-med-$TYP $FLAGS
+sutil_run_script.sh $SCRIPT.sh $INP $i reexec:t=scar,b=2,p=$PROB iact-med-$TYP $FLAGS
 
-sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.0 iact-med-$TYP $FLAGS
-sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.01 iact-med-$TYP $FLAGS
-sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.02 iact-med-$TYP $FLAGS
-sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.04 iact-med-$TYP $FLAGS
-sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.08 iact-med-$TYP $FLAGS
+sutil_run_script.sh $SCRIPT.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.0 iact-med-$TYP $FLAGS
+sutil_run_script.sh $SCRIPT.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.01 iact-med-$TYP $FLAGS
+sutil_run_script.sh $SCRIPT.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.02 iact-med-$TYP $FLAGS
+sutil_run_script.sh $SCRIPT.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.04 iact-med-$TYP $FLAGS
+sutil_run_script.sh $SCRIPT.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.08 iact-med-$TYP $FLAGS
 
 
 done
