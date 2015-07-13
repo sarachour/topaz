73e14617663d82f4f50b908359b7ecc2cf943c63
Sara AChour <sachour@mit.com>
2015-07-13 12:43:12 -0700

    Added barnes to mix

---
+++ benchmarks/barnes/run_barn_batch.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+INP=4K
+TYP=static
+PROB=0.01
+BLOCKSIZE=5
+#STARTSEED=1
+#NSEEDS=3
+STARTSEED=1
+NSEEDS=8
+FLAGS=d
+
+for i in $(seq $STARTSEED $NSEEDS);
+do
+
+sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=5,p=$PROB iact-med-$TYP $FLAGS
+sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=4,p=$PROB iact-med-$TYP $FLAGS
+sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=3,p=$PROB iact-med-$TYP $FLAGS
+sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=2,p=$PROB iact-med-$TYP $FLAGS
+
+sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.0 iact-med-$TYP $FLAGS
+sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.01 iact-med-$TYP $FLAGS
+sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.02 iact-med-$TYP $FLAGS
+sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.04 iact-med-$TYP $FLAGS
+sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.08 iact-med-$TYP $FLAGS
+
+
+done
