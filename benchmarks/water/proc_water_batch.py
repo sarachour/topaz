c6985f9c393b1f9ccf7a15618ed0c51b5c24780f
Sara Achour <sachour@mit.edu>
2015-07-13 08:37:25 -0700

    proper error bars

--- benchmarks/water/proc_water_batch.py
+++ benchmarks/water/proc_water_batch.py
@@ -34,10 +34,11 @@ for line in file:
 		if not (prob in plot[typ][bs]):
 			plot[typ][bs][prob]={}
 				
-		avg = scipy.mean(data)
-		stdev = scipy.std(data)
-		plot[typ][bs][prob]["avg"] = (avg)
-		plot[typ][bs][prob]["stdev"] = (stdev)
+		#stdev = scipy.std(data)
+		avg = numpy.percentile(data, 50)
+		plot[typ][bs][prob]["median"] =avg 
+		plot[typ][bs][prob]["low"] = avg-numpy.percentile(data, 25)
+		plot[typ][bs][prob]["high"] = numpy.percentile(data, 75)-avg
 		
 		
 	except ValueError:
@@ -45,17 +46,10 @@ for line in file:
 
 
 def q_bs(v):
-	res={}
-	print v,plot["normal"][v][0.01]
-	res["avg"] = plot["normal"][v][0.01]["avg"] 
-	res["std"] = plot["normal"][v][0.01]["stdev"] 
-	return res 
+	return plot["normal"][v][0.01] 
 	
 def q_pr(v):
-	res={}
-	res["avg"] = plot["normal"][5][v]["avg"] 
-	res["std"] = plot["normal"][5][v]["stdev"] 
-	return res
+	return  plot["normal"][5][v]
 
 
 
@@ -63,16 +57,18 @@ plt.figure()
 plt.title("Block Size vs Percent Error")
 indep = [1,2,3,4]
 data= map(lambda x : q_bs(x+1),indep)
-avg = map(lambda x : x["avg"],data) 
-std = map(lambda x : x["std"],data) 
-plt.errorbar(indep, avg, yerr=std, fmt='o--')
+avg = map(lambda x : x["median"],data) 
+lw = map(lambda x : x["low"],data) 
+hi = map(lambda x : x["high"],data) 
+plt.errorbar(indep, avg, yerr=[lw,hi], fmt='o--')
 plt.savefig("bs.png")
 
 plt.figure()
 plt.title("Target Probability vs Percent Error")
 indep = [0,0.01,0.02,0.04]
 data= map(lambda x : q_pr(x),indep)
-avg = map(lambda x : x["avg"],data) 
-std = map(lambda x : x["std"],data) 
-plt.errorbar(indep, avg, yerr=std, fmt='o--')
+avg = map(lambda x : x["median"],data) 
+lw = map(lambda x : x["low"],data) 
+hi = map(lambda x : x["high"],data) 
+plt.errorbar(indep, avg, yerr=[lw,hi], fmt='o--')
 plt.savefig("targ-prob.png")
