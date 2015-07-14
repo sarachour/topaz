648a0884f6210fd50a6b1418af732896bf47bdb7
Sara AChour <sachour@mit.com>
2015-07-14 12:22:39 -0700

    Added selection infomration

--- benchmarks/aov/run_aov_batch.sh
+++ benchmarks/aov/run_aov_batch.sh
@@ -6,7 +6,15 @@ FLAGS=
 
 #./run_aov.sh correct none $INPUT $SEED
 ./run_aov.sh normalization naive $INPUT $SEED reexec:t=scar,b=5,p=0.01 iact-$TYP $FLAGS
-exit 0
 ./run_aov.sh normalization subtract $INPUT $SEED reexec:t=scar,b=5,p=0.01 iact-$TYP $FLAGS
 ./run_aov.sh normalization arbitrarge $INPUT $SEED reexec:t=scar,b=5,p=0.01 iact-$TYP $FLAGS
 ./run_aov.sh normalization unknown $INPUT $SEED reexec:t=scar,b=5,p=0.01 iact-$TYP $FLAGS
+
+./run_aov.sh batching batch1 $INPUT $SEED reexec:t=scar,b=5,p=0.01 iact-$TYP $FLAGS
+./run_aov.sh batching batch2 $INPUT $SEED reexec:t=scar,b=5,p=0.01 iact-$TYP $FLAGS
+./run_aov.sh batching batch4 $INPUT $SEED reexec:t=scar,b=5,p=0.01 iact-$TYP $FLAGS
+./run_aov.sh batching batch8 unknown $INPUT $SEED reexec:t=scar,b=5,p=0.01 iact-$TYP $FLAGS
+
+./run_aov.sh selection out $INPUT $SEED reexec:t=scar,b=5,p=0.01 iact-$TYP $FLAGS
+./run_aov.sh selection inout $INPUT $SEED reexec:t=scar,b=5,p=0.01 iact-$TYP $FLAGS
+./run_aov.sh selection all $INPUT $SEED reexec:t=scar,b=5,p=0.01 iact-$TYP $FLAGS
