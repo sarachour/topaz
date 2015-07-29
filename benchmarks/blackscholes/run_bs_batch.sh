5d060f582e3f77488b9b8db6da26e395055dfac6
Ubuntu <ubuntu@ip-10-0-0-174.ec2.internal>
2015-07-29 19:53:17 +0000

    fixed bs

--- benchmarks/blackscholes/run_bs_batch.sh
+++ benchmarks/blackscholes/run_bs_batch.sh
@@ -3,7 +3,7 @@
 INP=16K
 #TYP=med-static2
 #TYP=med-static
-TYP=med-static
+TYP=med-dynamic
 PROB=0.01
 BLOCKSIZE=2
 
@@ -19,7 +19,7 @@ echo "input: $INP" >> tmp.txt
 
 #mutt -s "Blackscholes Started" sarachour@gmail.com < tmp.txt
 
-#sutil_run_script.sh run_bs.sh $INP 1 none perfect
+sutil_run_script.sh run_bs.sh $INP 1 none perfect
 for i in $(seq $STARTSEED $NSEEDS);
 do
 
@@ -32,20 +32,20 @@ FLAGS=t
 sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.00 iact-$TYP $FLAGS
 
 FLAGS=d
-sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.01 iact-$TYP $FLAGS
-sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.02 iact-$TYP $FLAGS
-sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.04 iact-$TYP $FLAGS
-sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.08 iact-$TYP $FLAGS
-sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.08 iact-$TYP $FLAGS
-sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.16 iact-$TYP $FLAGS
+#sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.01 iact-$TYP $FLAGS
+#sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.02 iact-$TYP $FLAGS
+#sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.04 iact-$TYP $FLAGS
+#sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.08 iact-$TYP $FLAGS
+#sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.08 iact-$TYP $FLAGS
+#sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.16 iact-$TYP $FLAGS
 
 FLAGS=t
-sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.01 iact-$TYP $FLAGS
-sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.02 iact-$TYP $FLAGS
-sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.04 iact-$TYP $FLAGS
-sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.08 iact-$TYP $FLAGS
-sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.11 iact-$TYP $FLAGS
-sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.16 iact-$TYP $FLAGS
+#sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.01 iact-$TYP $FLAGS
+#sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.02 iact-$TYP $FLAGS
+#sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.04 iact-$TYP $FLAGS
+#sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.08 iact-$TYP $FLAGS
+#sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.11 iact-$TYP $FLAGS
+#sutil_run_script.sh run_bs.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.16 iact-$TYP $FLAGS
 
 
 
