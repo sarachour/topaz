326b4d085730980b6062760ba421bf85c98ca815
Sara AChour <sachour@mit.com>
2015-07-30 07:48:38 -0700

    fixed bs aov

--- benchmarks/aov/run_aov_batch.sh
+++ benchmarks/aov/run_aov_batch.sh
@@ -3,29 +3,50 @@ SEED=1
 TYP=med-static
 STARTSEED=1
 NSEEDS=1
-FLAGS=d
 
 #echo "aov batch is beginning execution." > tmp.txt
 #echo "flags $FLAGS" >> tmp.txt
 #mutt -s "AOV Started" sarachour@gmail.com < tmp.txt
 
-#./run_aov.sh correct none $INPUT $SEED || exit 1
+./run_aov.sh correct none $INPUT 1 || exit 1
 echo "starting run"
 
 for SEED in $(seq $STARTSEED $NSEEDS);
 do
+	FLAGS=d
 	echo "seed $SEED"
 	./run_aov.sh normalization naive $INPUT $SEED reexec:t=scar,b=2,p=0.00 iact-$TYP $FLAGS || exit 1
 	#./run_aov.sh normalization subtract $INPUT $SEED reexec:t=scar,b=2,p=0.00 iact-$TYP $FLAGS || exit 1
 	./run_aov.sh normalization arbitrarge $INPUT $SEED reexec:t=scar,b=2,p=0.00 iact-$TYP $FLAGS || exit 1
+	
+	
+	#./run_aov.sh selection out $INPUT $SEED reexec:t=scar,b=2,p=0.00 iact-$TYP $FLAGS || exit 1
+	#./run_aov.sh selection inout $INPUT $SEED reexec:t=scar,b=2,p=0.00 iact-$TYP $FLAGS || exit 1
+	#./run_aov.sh selection all $INPUT $SEED reexec:t=scar,b=2,p=0.00 iact-$TYP $FLAGS || exit 1
+	#./run_aov.sh selection strikeout $INPUT $SEED reexec:t=scar,b=2,p=0.00 iact-$TYP $FLAGS || exit 1
+	#./run_aov.sh selection timeout $INPUT $SEED reexec:t=scar,b=2,p=0.00 iact-$TYP $FLAGS || exit 1
+	#./run_aov.sh selection rateout $INPUT $SEED reexec:t=scar,b=2,p=0.00 iact-$TYP $FLAGS || exit 1
+	#./run_aov.sh selection typeout $INPUT $SEED reexec:t=scar,b=2,p=0.00 iact-$TYP $FLAGS || exit 1
 
-	./run_aov.sh selection out $INPUT $SEED reexec:t=scar,b=2,p=0.00 iact-$TYP $FLAGS || exit 1
-	./run_aov.sh selection inout $INPUT $SEED reexec:t=scar,b=2,p=0.00 iact-$TYP $FLAGS || exit 1
-	./run_aov.sh selection all $INPUT $SEED reexec:t=scar,b=2,p=0.00 iact-$TYP $FLAGS || exit 1
-	./run_aov.sh selection strikeout $INPUT $SEED reexec:t=scar,b=2,p=0.00 iact-$TYP $FLAGS || exit 1
-	./run_aov.sh selection timeout $INPUT $SEED reexec:t=scar,b=2,p=0.00 iact-$TYP $FLAGS || exit 1
-	./run_aov.sh selection rateout $INPUT $SEED reexec:t=scar,b=2,p=0.00 iact-$TYP $FLAGS || exit 1
-	./run_aov.sh selection typeout $INPUT $SEED reexec:t=scar,b=2,p=0.00 iact-$TYP $FLAGS || exit 1
+	#./run_aov.sh batching batch1 $INPUT $SEED reexec:t=scar,b=2,p=0.00 iact-$TYP $FLAGS || exit 1
+	#./run_aov.sh batching batch2 $INPUT $SEED reexec:t=scar,b=2,p=0.00 iact-$TYP $FLAGS || exit 1
+	#./run_aov.sh batching batch4 $INPUT $SEED reexec:t=scar,b=2,p=0.00 iact-$TYP $FLAGS || exit 1
+	#./run_aov.sh batching batch8l $INPUT $SEED reexec:t=scar,b=2,p=0.00 iact-$TYP $FLAGS || exit 1
+	#./run_aov.sh batching batch64 $INPUT $SEED reexec:t=scar,b=2,p=0.00 iact-$TYP $FLAGS || exit 1
+	
+	FLAGS=t
+	#./run_aov.sh normalization naive $INPUT $SEED reexec:t=scar,b=2,p=0.00 iact-$TYP $FLAGS || exit 1
+	#./run_aov.sh normalization subtract $INPUT $SEED reexec:t=scar,b=2,p=0.00 iact-$TYP $FLAGS || exit 1
+	#./run_aov.sh normalization arbitrarge $INPUT $SEED reexec:t=scar,b=2,p=0.00 iact-$TYP $FLAGS || exit 1
+	
+	
+	#./run_aov.sh selection out $INPUT $SEED reexec:t=scar,b=2,p=0.00 iact-$TYP $FLAGS || exit 1
+	#./run_aov.sh selection inout $INPUT $SEED reexec:t=scar,b=2,p=0.00 iact-$TYP $FLAGS || exit 1
+	#./run_aov.sh selection all $INPUT $SEED reexec:t=scar,b=2,p=0.00 iact-$TYP $FLAGS || exit 1
+	#./run_aov.sh selection strikeout $INPUT $SEED reexec:t=scar,b=2,p=0.00 iact-$TYP $FLAGS || exit 1
+	#./run_aov.sh selection timeout $INPUT $SEED reexec:t=scar,b=2,p=0.00 iact-$TYP $FLAGS || exit 1
+	#./run_aov.sh selection rateout $INPUT $SEED reexec:t=scar,b=2,p=0.00 iact-$TYP $FLAGS || exit 1
+	#./run_aov.sh selection typeout $INPUT $SEED reexec:t=scar,b=2,p=0.00 iact-$TYP $FLAGS || exit 1
 
 	#./run_aov.sh batching batch1 $INPUT $SEED reexec:t=scar,b=2,p=0.00 iact-$TYP $FLAGS || exit 1
 	#./run_aov.sh batching batch2 $INPUT $SEED reexec:t=scar,b=2,p=0.00 iact-$TYP $FLAGS || exit 1
