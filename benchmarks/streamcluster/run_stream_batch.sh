160f8249534a02810d3502b9e7ce07f3aed62409
Ubuntu <ubuntu@ip-10-0-0-174.ec2.internal>
2015-08-06 16:37:55 +0000

    added figures

--- benchmarks/streamcluster/run_stream_batch.sh
+++ benchmarks/streamcluster/run_stream_batch.sh
@@ -19,20 +19,20 @@ echo "input: $INP" >> tmp.txt
 #mutt -s "Streamcluster Started" sarachour@gmail.com  < tmp.txt
 
 
-#sutil_run_script.sh $SCRIPT.sh $INP 1 none perfect 
+sutil_run_script.sh $SCRIPT.sh $INP 1 none perfect 
 
 
 for i in $(seq $STARTSEED $NSEEDS);
 do
 # 6 for normal,
-FLAGS=d
-sutil_run_script.sh $SCRIPT.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.00 iact-med-$TYP $FLAGS
+#FLAGS=d
+#sutil_run_script.sh $SCRIPT.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.00 iact-med-$TYP $FLAGS
 
-FLAGS=t
-sutil_run_script.sh $SCRIPT.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.00 iact-med-$TYP $FLAGS
+#FLAGS=t
+#sutil_run_script.sh $SCRIPT.sh $INP $i reexec:t=scar,b=$BLOCKSIZE,p=0.00 iact-med-$TYP $FLAGS
 
-FLAGS=t
-sutil_run_script.sh $SCRIPT.sh $INP $i none iact-med-$TYP $FLAGS
+#FLAGS=t
+#sutil_run_script.sh $SCRIPT.sh $INP $i none iact-med-$TYP $FLAGS
 
 #tried 6,7,8,4
 #SEED=5
